96798429761a65f6f7190995c8a2d84c76b7781d
Mona Lisa <clarkefraser1@gmail.com>
2021-04-02 16:27:10 +1100

    setting up server.js and models folder

---
+++ models/index.js
@@ -0,0 +1,9 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+  api: require("./api"),
+  exercise: require("./exercise"),
+  stats: require("./stats"),
+  workout: require("./workout"),
+  index: require("./index"),
+};
